df635a53bab3acd6fe13d6408d45e5346016a533
shota.takarabe <syota.takarabe@gmail.com>
2023-04-12 21:47:37 +0900

    componens化しました

---
+++ components/Center.tsx
@@ -0,0 +1,43 @@
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+
+
+export function Center() {
+    return (
+        <>
+            <div className={styles.description}>
+                <p>
+                    Indexページですよ！！！！
+                </p>
+                <div>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        By{' '}
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.vercelLogo}
+                            width={100}
+                            height={24}
+                            priority
+                        />
+                    </a>
+                </div>
+            </div>
+
+            <div className={styles.center}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </div>
+        </>
+    )
+}
---
+++ components/Fotter.tsx
@@ -0,0 +1,9 @@
+export function Fotter() {
+    return (
+        <footer>
+            <p>
+                Fotterさんのお出ましですよ〜〜〜
+            </p>
+        </footer>
+    )
+}
